3fa3449ac326d584809c33d8ba6715ca0e1cd5af
NanyaGupta <73479405+NanyaGupta@users.noreply.github.com>
2021-08-01 08:51:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+rightWristX=0;
+leftWristX=0;
+difference=0;
+
+
+function setup()  {
+    video = createCapture(VIDEO);
+    video.size(525, 525);
+
+    canvas = createCanvas(525,525);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+   console.log('Posenet has Intializied Successfully!');
+}
+
+function draw() {
+    background('#800000');
+    fill('#FFC0CB');
+    textSize(139);
+}
+
+function font(){
+    text("Nanya", 20,200)
+}
+
+function gotPoses(results) {
+    if(results.length > 0){
+       console.log(results);
+
+       leftWristX= results[0].pose.leftWrist.x;
+       rightWristX= results[0].pose.rightWrist.x;
+       difference = floor(leftWristX - rightWristX);
+       console.log("leftWrist = " + leftWristX + " rightWrist= " + rightWristX + " difference= " + difference);
+    }
+}
